3a23d833386190e35cfa67f390c894f60b4ad41e
ngoctin <ngoctint1lvc@gmail.com>
2024-10-31 00:18:14 +0700

    fix bug youtube font-size, move by subs netflix

--- src/utils/moveVideoToTime.ts
+++ src/utils/moveVideoToTime.ts
@@ -1,7 +1,7 @@
 import Service from "@src/streamings/service";
 
 export const moveVideoToTime = (video: HTMLVideoElement, streaming: Service, time: number) => {
-  if (streaming.constructor.name.toLowerCase() === "netflix") {
+  if (streaming.name.toLowerCase() === "netflix") {
     window.dispatchEvent(new CustomEvent("esNetflixSeek", { detail: time }));
   } else {
     video.currentTime = time / 1000;
